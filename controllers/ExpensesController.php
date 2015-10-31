03aaaed99035a91c0f9c9fbe8efe05ca0436eccc
Artem E <freelook00@yandex.ru>
2015-10-31 17:14:18 +0300

    Create CRUD over Gii

--- controllers/ExpensesController.php
+++ controllers/ExpensesController.php
@@ -2,41 +2,120 @@
 
 namespace app\controllers;
 
-class ExpensesController extends \yii\web\Controller
+use Yii;
+use app\models\Expense;
+use app\models\ExpenseQuery;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+
+/**
+ * ExpensesController implements the CRUD actions for Expense model.
+ */
+class ExpensesController extends Controller
 {
-    public function actionDelete()
+    public function behaviors()
     {
-        return $this->render('delete');
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['post'],
+                ],
+            ],
+        ];
     }
 
-    public function actionEdit()
-    {
-        return $this->render('edit');
-    }
-
-    public function actionExpense()
-    {
-        return $this->render('expense');
-    }
-
-    public function actionGetmonth()
-    {
-        return $this->render('getmonth');
-    }
-
+    /**
+     * Lists all Expense models.
+     * @return mixed
+     */
     public function actionIndex()
     {
-        return $this->render('index');
+        $searchModel = new ExpenseQuery();
+        $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
+
+        return $this->render('index', [
+            'searchModel' => $searchModel,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
-    public function actionNew()
+    /**
+     * Displays a single Expense model.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionView($id)
     {
-        return $this->render('new');
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
     }
 
-    public function actionReport()
+    /**
+     * Creates a new Expense model.
+     * If creation is successful, the browser will be redirected to the 'view' page.
+     * @return mixed
+     */
+    public function actionCreate()
     {
-        return $this->render('report');
+        $model = new Expense();
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
     }
 
+    /**
+     * Updates an existing Expense model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->id]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Expense model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param integer $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * Finds the Expense model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Expense the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Expense::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
--- models/ExpenseQuery.php
+++ models/ExpenseQuery.php
@@ -2,34 +2,68 @@
 
 namespace app\models;
 
-/**
- * This is the ActiveQuery class for [[Expense]].
- *
- * @see Expense
- */
-class ExpenseQuery extends \yii\db\ActiveQuery
-{
-    /*public function active()
-    {
-        $this->andWhere('[[status]]=1');
-        return $this;
-    }*/
+use Yii;
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Expense;
 
+/**
+ * ExpenseQuery represents the model behind the search form about `app\models\Expense`.
+ */
+class ExpenseQuery extends Expense
+{
     /**
      * @inheritdoc
-     * @return Expense[]|array
      */
-    public function all($db = null)
+    public function rules()
     {
-        return parent::all($db);
+        return [
+            [['id'], 'integer'],
+            [['date', 'comment'], 'safe'],
+            [['total'], 'number'],
+        ];
     }
 
     /**
      * @inheritdoc
-     * @return Expense|array|null
      */
-    public function one($db = null)
+    public function scenarios()
     {
-        return parent::one($db);
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Expense::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'date' => $this->date,
+            'total' => $this->total,
+        ]);
+
+        $query->andFilterWhere(['like', 'comment', $this->comment]);
+
+        return $dataProvider;
     }
 }
